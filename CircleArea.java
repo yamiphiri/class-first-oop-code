fd55bde217086af15071f39d5f973d9ed4360d68
Yamikani Phiri <yamikanidj@gmail.com>
2021-12-10 16:55:25 +0200

    area calculation

--- CircleArea.java
+++ CircleArea.java
@@ -5,6 +5,7 @@ public class CircleArea{
         //check if connnecting
         Circle c1 = new Circle();
         System.out.println(c1.radius);
+        System.out.println("areas is"+c1.getArea());
         //use the created class to calc area
     }
 }
